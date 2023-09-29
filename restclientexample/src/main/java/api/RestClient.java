13d40a5f5749db41d16115f4e12ed72938f78775
David Zhang <zdavid4525@gmail.com>
2023-09-29 09:51:30 -0400

    added bare bones implementation of rest client using placeholder api url

--- restclientexample/src/main/java/api/RestClient.java
+++ restclientexample/src/main/java/api/RestClient.java
@@ -1,5 +1,31 @@
 package api;
 
-public class RestClient {
+import okhttp3.*;
+import org.json.JSONException;
 
+import java.io.IOException;
+
+public class RestClient {
+    private static final String REST_URL = "https://random-data-api.com/api/v2/banks";
+
+    public static String getMarketData() throws RuntimeException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder()
+                .url(REST_URL) // Add headers here, use String.format to add query params
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            // we can serialize into json here but idk if we have to
+
+            String strResponse = response.body().string();
+            System.out.println(strResponse);
+            return strResponse;
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String marketDataStr = getMarketData();
+    }
 }
