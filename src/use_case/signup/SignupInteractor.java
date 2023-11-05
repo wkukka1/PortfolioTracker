de6ff97f0c5ed0f6888e7306497a967679737ccc
David Zhang <zdavid4525@gmail.com>
2023-11-05 15:07:45 -0500

    added logic to create and save a portfolio associated with this user's ID upon user signup

--- src/use_case/signup/SignupInteractor.java
+++ src/use_case/signup/SignupInteractor.java
@@ -1,19 +1,24 @@
 package use_case.signup;
 
+import entity.Portfolio;
 import entity.User;
 import entity.UserFactory;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class SignupInteractor implements SignupInputBoundary {
     final SignupUserDataAccessInterface userDataAccessObject;
+    final PortfolioDataAccessInterface portfolioDataAccessObject;
     final SignupOutputBoundary userPresenter;
     final UserFactory userFactory;
 
     public SignupInteractor(SignupUserDataAccessInterface signupDataAccessInterface,
+                            PortfolioDataAccessInterface portfolioDataAccessObject,
                             SignupOutputBoundary signupOutputBoundary,
                             UserFactory userFactory) {
         this.userDataAccessObject = signupDataAccessInterface;
+        this.portfolioDataAccessObject = portfolioDataAccessObject;
         this.userPresenter = signupOutputBoundary;
         this.userFactory = userFactory;
     }
@@ -25,10 +30,12 @@ public class SignupInteractor implements SignupInputBoundary {
         } else if (!signupInputData.getPassword().equals(signupInputData.getRepeatPassword())) {
             userPresenter.prepareFailView("Passwords don't match.");
         } else {
-
             LocalDateTime now = LocalDateTime.now();
             User user = userFactory.create(signupInputData.getUsername(), signupInputData.getPassword(), now, now.hashCode());
             userDataAccessObject.save(user);
+
+            Portfolio initialPortfolio = new Portfolio(new ArrayList<>(), 0L, user.getUserID());
+            portfolioDataAccessObject.savePortfolio(initialPortfolio);
 
             SignupOutputData signupOutputData = new SignupOutputData(user.getName(), now.toString(), false);
             userPresenter.prepareSuccessView(signupOutputData);
