bfbabe1078cd1b8a78f10da79e13cd8bc2f90add
David Zhang <zdavid4525@gmail.com>
2023-11-05 14:57:55 -0500

    created Portfolio DAO and added initial constructor

--- src/data_access/FilePortfolioDataAccessObject.java
+++ src/data_access/FilePortfolioDataAccessObject.java
@@ -1,2 +1,42 @@
-package data_access;public class FilePortfolioDataAccessObject {
+package data_access;
+
+import entity.Portfolio;
+import entity.Stock;
+import use_case.signup.PortfolioDataAccessInterface;
+
+import java.io.*;
+import java.util.*;
+
+public class FilePortfolioDataAccessObject implements PortfolioDataAccessInterface {
+    private final File csvFile;
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+    private final Map<Integer, Portfolio> portfolios = new HashMap<>();
+
+    public FilePortfolioDataAccessObject(String csvPath) throws IOException {
+        this.csvFile = new File(csvPath);
+        headers.put("userID", 0);
+        headers.put("netWorth", 1);
+        headers.put("stockList", 2);
+
+        if (csvFile.length() == 0) {
+            save();
+        } else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String row;
+                reader.readLine();
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",", 3);
+
+                    int userID = Integer.parseInt(String.valueOf(col[headers.get("userID")]));
+                    long netWorth = Long.parseLong(String.valueOf(col[headers.get("netWorth")]));
+
+                    String encodedStocks = String.valueOf(col[headers.get("stockList")]);
+                    ArrayList<Stock> decodedStockList = decodeStockStrIntoStockList(encodedStocks);
+
+                    Portfolio currPortfolio = new Portfolio(decodedStockList, netWorth, userID);
+                    portfolios.put(userID, currPortfolio);
+                }
+            }
+        }
+    }
 }
