0021afb0f4194d14de0ec200d3028e1f6f637957
Will Kukkamalla <dogplate.jr@gmail.com>
2023-11-17 21:36:28 -0500

    Started a function to delete the users and porfolios accts and added the Timer Class and count Lines funciton also changed the structure of LabelTextPanel to make it easier to check

--- src/main/java/view/LabelTextPanel.java
+++ src/main/java/view/LabelTextPanel.java
@@ -6,8 +6,14 @@ import javax.swing.*;
  * A panel containing a label and a text field.
  */
 class LabelTextPanel extends JPanel {
+    private JLabel label;
     LabelTextPanel(JLabel label, JTextField textField) {
+        this.label = label;
         this.add(label);
         this.add(textField);
+    }
+
+    public JLabel getLabel() {
+        return label;
     }
 }
--- test/view/DeleteUsersTest.java
+++ test/view/DeleteUsersTest.java
@@ -23,6 +23,7 @@ import static org.junit.Assert.*;
 public class DeleteUsersTest {
     static String message = "";
     static boolean popUpDiscovered = false;
+
     public void addUser(int numOfUsers) {
         UserFactory uf = new CommonUserFactory();
         FileUserDataAccessObject fudao;
@@ -62,12 +63,120 @@ public class DeleteUsersTest {
         return (JButton) buttons.getComponent(0);
     }
 
+    public LabelTextPanel getTextFeilds() {
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+        assertNotNull(app);
+
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        // Assuming LoginView is at index 1 in the JPanel
+        LoginView lv = (LoginView) jp2.getComponent(1);
+
+        LabelTextPanel textField = (LabelTextPanel) lv.getComponent(3);
+
+        // Assuming logIn button is at index 0 in the buttons
+        return textField;
+    }
+
+    public boolean deleteAccount(int num) {
+        addUser(num);
+        JButton loginBtn = getLoginBtn();
+        int initalUsers;
+        int intialPortfolios;
+        int finalUsers;
+        int finalPortfolios;
+        createCloseTimer().start();
+
+        loginBtn.doClick();
+        try {
+            initalUsers = countLines("./users.csv");
+            intialPortfolios = countLines("./portfolios.csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        try {
+            finalUsers = countLines("./users.csv");
+            finalPortfolios = countLines("./portfolios.csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return initalUsers == finalUsers + num && intialPortfolios == finalPortfolios + num;
+    }
+
+
     @org.junit.Test
-    public void testGetLoginBtn(){
+    public void testGetLoginBtn() {
         Main.main(null);
         JButton button = getLoginBtn();
         System.out.println(button.getText());
-        assert(button.getText().equals("Log in"));
+        assert (button.getText().equals("Log in"));
     }
 
+    @org.junit.Test
+    public void testGetTextFeilds() {
+        Main.main(null);
+        LabelTextPanel textField = getTextFeilds();
+        System.out.println(textField.getLabel().getText());
+        assert (textField.getLabel().getText().equals("Password"));
+    }
+
+    private Timer createCloseTimer() {
+        ActionListener close = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Window[] windows = Window.getWindows();
+                for (Window window : windows) {
+
+                    if (window instanceof JDialog) {
+
+                        JDialog dialog = (JDialog) window;
+
+                        // this ignores old dialogs
+                        if (dialog.isVisible()) {
+                            String s = ((JOptionPane) ((BorderLayout) dialog.getRootPane()
+                                    .getContentPane().getLayout()).getLayoutComponent(BorderLayout.CENTER)).getMessage().toString();
+                            System.out.println("message = " + s);
+
+                            // store the information we got from the JDialog
+                            DeleteUsersTest.message = s;
+                            DeleteUsersTest.popUpDiscovered = true;
+
+                            System.out.println("disposing of..." + window.getClass());
+                            window.dispose();
+                        }
+                    }
+                }
+            }
+
+        };
+
+        Timer t = new Timer(1000, close);
+        t.setRepeats(false);
+        return t;
+    }
+
+    private static int countLines(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        int lineCount = 0;
+        while (reader.readLine() != null) {
+            lineCount++;
+        }
+        return lineCount;
+    }
+
+
 }
+
