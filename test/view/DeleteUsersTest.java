37ae2d37b24392307fc652c59955277d77b663bd
Will Kukkamalla <dogplate.jr@gmail.com>
2023-11-17 20:57:08 -0500

    Added a tests folder and created a method to add users to the user file and portfolio file, made a functio nto get the login button

--- test/view/DeleteUsersTest.java
+++ test/view/DeleteUsersTest.java
@@ -1,2 +1,73 @@
-package view;public class DeleteUsersTest {
+package view;
+
+import app.Main;
+import app.SignupUseCaseFactory;
+import data_access.FilePortfolioDataAccessObject;
+import data_access.FileUserDataAccessObject;
+import entity.CommonUserFactory;
+import entity.Portfolio;
+import entity.UserFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class DeleteUsersTest {
+    static String message = "";
+    static boolean popUpDiscovered = false;
+    public void addUser(int numOfUsers) {
+        UserFactory uf = new CommonUserFactory();
+        FileUserDataAccessObject fudao;
+        FilePortfolioDataAccessObject fpdao;
+        try {
+            fudao = new FileUserDataAccessObject("./users.csv", uf);
+            fpdao = new FilePortfolioDataAccessObject("./portfolios.csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 0; i < numOfUsers; i++) {
+            fudao.save(uf.create("user" + i, "password" + i, LocalDateTime.now(), i));
+            fpdao.savePortfolio(new Portfolio(new ArrayList<>(), 0, i));
+        }
+    }
+
+    public JButton getLoginBtn() {
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+        assertNotNull(app);
+
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        // Assuming LoginView is at index 1 in the JPanel
+        LoginView lv = (LoginView) jp2.getComponent(1);
+
+        JPanel buttons = (JPanel) lv.getComponent(5);
+
+        // Assuming logIn button is at index 0 in the buttons JPanel
+        return (JButton) buttons.getComponent(0);
+    }
+
+    @org.junit.Test
+    public void testGetLoginBtn(){
+        Main.main(null);
+        JButton button = getLoginBtn();
+        System.out.println(button.getText());
+        assert(button.getText().equals("Log in"));
+    }
+
 }
