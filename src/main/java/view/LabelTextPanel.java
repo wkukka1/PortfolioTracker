1a46e0a25675f2ee7b728185dfe4f47b07b2a1a0
Will Kukkamalla <dogplate.jr@gmail.com>
2023-11-19 18:12:11 -0500

    Added some button getters and the first test for deleting users accounts changed LabelTextPanel so that I could input text into the text feilds for the tests

--- src/main/java/view/LabelTextPanel.java
+++ src/main/java/view/LabelTextPanel.java
@@ -7,13 +7,19 @@ import javax.swing.*;
  */
 class LabelTextPanel extends JPanel {
     private JLabel label;
+    private JTextField textField;
     LabelTextPanel(JLabel label, JTextField textField) {
         this.label = label;
+        this.textField = textField;
         this.add(label);
         this.add(textField);
     }
 
     public JLabel getLabel() {
         return label;
+    }
+
+    public void setText(String text){
+        textField.setText(text);
     }
 }
--- test/view/DeleteUsersTest.java
+++ test/view/DeleteUsersTest.java
@@ -1,7 +1,6 @@
 package view;
 
 import app.Main;
-import app.SignupUseCaseFactory;
 import data_access.FilePortfolioDataAccessObject;
 import data_access.FileUserDataAccessObject;
 import entity.CommonUserFactory;
@@ -63,7 +62,7 @@ public class DeleteUsersTest {
         return (JButton) buttons.getComponent(0);
     }
 
-    public LabelTextPanel[] getTextFeilds() {
+    public LabelTextPanel[] getTextFields() {
         JFrame app = null;
         Window[] windows = Window.getWindows();
         for (Window window : windows) {
@@ -86,22 +85,55 @@ public class DeleteUsersTest {
         return new LabelTextPanel[]{usernameTextField, passwordTextField};
     }
 
+    public JButton getDeleteBtn(){
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+        assertNotNull(app);
+
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        // Assuming LoginView is at index 1 in the JPanel
+        LoggedInView lv = (LoggedInView) jp2.getComponent(2);
+
+        return (JButton) lv.getComponent(5);
+    }
+
+
     public boolean deleteAccount(int num) {
-        addUser(num);
+        num = num -1;
+        //get all the buttons
         JButton loginBtn = getLoginBtn();
+        LabelTextPanel[] textFields = getTextFields();
+        JButton deleteBtn = getDeleteBtn();
+
         int initalUsers;
         int intialPortfolios;
         int finalUsers;
         int finalPortfolios;
-        createCloseTimer().start();
 
-        loginBtn.doClick();
         try {
             initalUsers = countLines("./users.csv");
             intialPortfolios = countLines("./portfolios.csv");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        textFields[0].setText("user"+String.valueOf(num));
+        textFields[1].setText("password"+String.valueOf(num));
+        createCloseTimer().start();
+
+        loginBtn.doClick();
+
+        createCloseTimer().start();
+
+        deleteBtn.doClick();
 
 
         try {
@@ -111,7 +143,7 @@ public class DeleteUsersTest {
             throw new RuntimeException(e);
         }
 
-        return initalUsers == finalUsers + num && intialPortfolios == finalPortfolios + num;
+        return initalUsers == finalUsers + num + 1 && intialPortfolios == finalPortfolios + num + 1;
     }
 
 
@@ -126,10 +158,28 @@ public class DeleteUsersTest {
     @org.junit.Test
     public void testGetTextFeilds() {
         Main.main(null);
-        LabelTextPanel[] textField = getTextFeilds();
+        LabelTextPanel[] textField = getTextFields();
         System.out.println(textField[0].getLabel().getText());
         assert (textField[0].getLabel().getText().equals("Username") &&
                 textField[1].getLabel().getText().equals("Password"));
+    }
+
+    @org.junit.Test
+    public void testGetDeleteBtn() {
+        Main.main(null);
+        JButton button = getDeleteBtn();
+        System.out.println(button.getText());
+        assert (button.getText().equals("Delete Account"));
+    }
+
+
+
+    @org.junit.Test
+    public void testDeleteOneAccount(){
+        Main.main(null);
+        addUser(1);
+        assert deleteAccount(1);
+
     }
 
     private Timer createCloseTimer() {
