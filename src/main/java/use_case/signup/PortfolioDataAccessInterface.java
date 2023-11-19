6d85c96b67103602f67c992ed6a400b439bf19de
alexlee27 <alexlee7942@gmail.com>
2023-11-19 18:14:20 -0500

    Reformatting file structure

--- src/main/java/use_case/signup/PortfolioDataAccessInterface.java
+++
@@ -1,8 +0,0 @@
-package use_case.signup;
-
-import entity.Portfolio;
-
-public interface PortfolioDataAccessInterface {
-    void savePortfolio(Portfolio currPortfolio);
-    Portfolio getPortfolioByID(int userID);
-}
