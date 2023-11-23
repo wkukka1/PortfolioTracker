ed6c0f53edf6ce96d072d6924d71446e4c770b49
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-11-23 13:18:48 -0500

    implemented the remove stock use case Output Data initializer adn the getStockList() method

--- src/main/java/use_case/removeStock/RemoveStockOutputData.java
+++ src/main/java/use_case/removeStock/RemoveStockOutputData.java
@@ -1,2 +1,15 @@
-package use_case.removeStock;public class RemoveStockOutputData {
+package use_case.removeStock;
+
+import entity.Stock;
+import java.util.List;
+public class RemoveStockOutputData {
+    private final List<Stock> stockList;
+
+    RemoveStockOutputData(List<Stock> stockList){
+        this.stockList = stockList;
+    }
+
+    public List<Stock> getStockList(){
+        return this.stockList;
+    }
 }
