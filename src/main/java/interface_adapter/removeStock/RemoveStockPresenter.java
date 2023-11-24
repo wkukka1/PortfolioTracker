65ca0c8567c28414ed722c0936115ef36192c0e6
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-11-24 10:38:59 -0500

    implemented prepareFailView

--- src/main/java/interface_adapter/removeStock/RemoveStockPresenter.java
+++ src/main/java/interface_adapter/removeStock/RemoveStockPresenter.java
@@ -1,2 +1,39 @@
-package interface_adapter.removeStock;public class RemoveStockPresenter {
+package interface_adapter.removeStock;
+
+import interface_adapter.logged_in.LoggedInState;
+import interface_adapter.logged_in.LoggedInViewModel;
+import use_case.removeStock.RemoveStockOutputBoundary;
+import interface_adapter.ViewManagerModel;
+import use_case.removeStock.RemoveStockOutputBoundary;
+import use_case.removeStock.RemoveStockOutputData;
+import view.LoggedInView;
+
+import javax.swing.*;
+
+public class RemoveStockPresenter implements RemoveStockOutputBoundary {
+    private final RemoveStockViewModel removeStockViewModel;
+    private final LoggedInViewModel loggedInViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public RemoveStockPresenter(ViewManagerModel viewManagerModel,
+                                RemoveStockViewModel removeStockViewModel,
+                                LoggedInViewModel loggedInViewModel){
+        this.viewManagerModel = viewManagerModel;
+        this.removeStockViewModel = removeStockViewModel;
+        this.loggedInViewModel = loggedInViewModel;
+    }
+    @Override
+    public void prepareSuccessView(RemoveStockOutputData response){
+        LoggedInState loggedInState = loggedInViewModel.getState();
+        this.loggedInViewModel.setState(loggedInState);
+        loggedInViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(loggedInViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+    @Override
+    public void prepareFailView(String error){
+        JOptionPane.showMessageDialog(null, "There was an error in deleting your account",
+                "Error", JOptionPane.ERROR_MESSAGE);
+    }
 }
