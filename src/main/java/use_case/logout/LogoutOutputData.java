d419d4765c267527f85b5ef618a0cb090da5ebde
Will Kukkamalla <dogplate.jr@gmail.com>
2023-11-25 00:21:07 -0500

    created the architecture for the logout use case

---
+++ src/main/java/use_case/logout/LogoutOutputData.java
@@ -0,0 +1,13 @@
+package use_case.logout;
+
+public class LogoutOutputData implements LogoutOutputBoundary{
+    @Override
+    public void prepareSuccessView() {
+
+    }
+
+    @Override
+    public void prepareFailView() {
+
+    }
+}
