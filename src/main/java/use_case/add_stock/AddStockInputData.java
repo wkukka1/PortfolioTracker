9a48fdfc6dd664495f2f59fe5680f81e88992202
David Zhang <zdavid4525@gmail.com>
2023-11-26 00:48:15 -0500

    added AddStockInputData object class w getters and setters

---
+++ src/main/java/use_case/add_stock/AddStockInputData.java
@@ -0,0 +1,33 @@
+package use_case.add_stock;
+
+import java.time.LocalDateTime;
+
+public class AddStockInputData {
+    private String tickerSymbol;
+    private LocalDateTime purchaseLocalDateTime;
+    private double totalValueAtPurchase;
+
+    public String getTickerSymbol() {
+        return tickerSymbol;
+    }
+
+    public void setTickerSymbol(String tickerSymbol) {
+        this.tickerSymbol = tickerSymbol;
+    }
+
+    public LocalDateTime getPurchaseLocalDateTime() {
+        return purchaseLocalDateTime;
+    }
+
+    public void setPurchaseLocalDateTime(LocalDateTime purchaseLocalDateTime) {
+        this.purchaseLocalDateTime = purchaseLocalDateTime;
+    }
+
+    public double getTotalValueAtPurchase() {
+        return totalValueAtPurchase;
+    }
+
+    public void setTotalValueAtPurchase(double totalValueAtPurchase) {
+        this.totalValueAtPurchase = totalValueAtPurchase;
+    }
+}
