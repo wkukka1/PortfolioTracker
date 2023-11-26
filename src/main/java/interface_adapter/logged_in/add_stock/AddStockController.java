06e2d379a6447dde066505342b72802b1175115f
David Zhang <zdavid4525@gmail.com>
2023-11-26 13:59:25 -0500

    implemented AddStockController.execute

--- src/main/java/interface_adapter/logged_in/add_stock/AddStockController.java
+++ src/main/java/interface_adapter/logged_in/add_stock/AddStockController.java
@@ -1,6 +1,10 @@
 package interface_adapter.logged_in.add_stock;
 
 import use_case.add_stock.AddStockInputBoundary;
+import use_case.add_stock.AddStockInputData;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class AddStockController {
     private final AddStockInputBoundary addStockInteractor;
@@ -10,6 +14,13 @@ public class AddStockController {
     }
 
     public void execute(String ticker, String date, String amount) {
-        
+        String[] dateComponents = date.split("/");
+        LocalDateTime purchaseLocalDateTime = LocalDate.of(Integer.parseInt(dateComponents[2]),
+                Integer.parseInt(dateComponents[1]), Integer.parseInt(dateComponents[0])).atStartOfDay();
+
+        AddStockInputData addStockInputData = new AddStockInputData(ticker, purchaseLocalDateTime,
+                Double.parseDouble(amount));
+
+        addStockInteractor.addStock(addStockInputData);
     }
 }
