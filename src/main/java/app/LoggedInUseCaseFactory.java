28fd4348b307c93fcef16a1e1d1e5f69d0736ac6
David Zhang <zdavid4525@gmail.com>
2023-11-26 01:03:30 -0500

    created LoggedInUseCaseFactory class to migrate separate creation logic from Main and implemented initial create, createAddStockUseCase methods

--- src/main/java/app/LoggedInUseCaseFactory.java
+++ src/main/java/app/LoggedInUseCaseFactory.java
@@ -1,2 +1,26 @@
-package app;public class LoggedInUseCaseFactory {
+package app;
+
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.logged_in.add_stock.AddStockController;
+import use_case.add_stock.AddStockInputBoundary;
+import use_case.add_stock.AddStockInteractor;
+import view.LoggedInView;
+import view.validation.StockFieldValidator;
+import view.validation.StockFieldValidatorImpl;
+
+public class LoggedInUseCaseFactory {
+    private LoggedInUseCaseFactory() {
+    }
+
+    public static LoggedInView create(LoggedInViewModel loggedInViewModel) {
+        AddStockController addStockController = createAddStockUseCase();
+        StockFieldValidator stockFieldValidator = new StockFieldValidatorImpl();
+
+        return new LoggedInView(loggedInViewModel, stockFieldValidator, addStockController);
+    }
+
+    private static AddStockController createAddStockUseCase() {
+        AddStockInputBoundary addStockInteractor = new AddStockInteractor();
+        return new AddStockController(addStockInteractor);
+    }
 }
