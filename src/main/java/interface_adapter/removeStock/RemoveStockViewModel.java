e2e60e185fb8eb86cd0416057109a3687e883101
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-11-26 17:12:29 -0500

    implemented the RemoveStockViewModel

--- src/main/java/interface_adapter/removeStock/RemoveStockViewModel.java
+++ src/main/java/interface_adapter/removeStock/RemoveStockViewModel.java
@@ -1,2 +1,25 @@
-package interface_adapter.removeStock;public class RemoveStockViewModel {
+package interface_adapter.removeStock;
+
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+public class RemoveStockViewModel extends ViewModel {
+    private RemoveStockState state = new RemoveStockState();
+
+    public final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public RemoveStockViewModel(){super("removeStock");}
+
+    @Override
+    public void firePropertyChanged(){
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener){
+        support.addPropertyChangeListener(listener);
+    }
+
+    public RemoveStockState getState(){return this.state;}
 }
