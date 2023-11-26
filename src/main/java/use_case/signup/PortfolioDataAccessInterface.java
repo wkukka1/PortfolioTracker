938056a4f00cf81a640f61cbcb0d673bc1a459ac
David Zhang <zdavid4525@gmail.com>
2023-11-26 16:02:50 -0500

    added abstract addStockToPortfolioByID to PortfolioDataAccessInterface

--- src/main/java/use_case/signup/PortfolioDataAccessInterface.java
+++ src/main/java/use_case/signup/PortfolioDataAccessInterface.java
@@ -1,8 +1,10 @@
 package use_case.signup;
 
 import entity.Portfolio;
+import entity.Stock;
 
 public interface PortfolioDataAccessInterface {
     void savePortfolio(Portfolio currPortfolio);
     Portfolio getPortfolioByID(int userID);
+    void addStockToPortfolioByID(int userID, Stock newStock, double stockProfitToDate);
 }
