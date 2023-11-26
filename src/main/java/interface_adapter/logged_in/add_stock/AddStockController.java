7662381393113c473a85a76487f6bb362d6e3003
David Zhang <zdavid4525@gmail.com>
2023-11-26 00:43:55 -0500

    changed class/constructor attribute types to AddStockInputBoundary (interface-to-be) instead of AddStockInteractor (implementation)

--- src/main/java/interface_adapter/logged_in/add_stock/AddStockController.java
+++ src/main/java/interface_adapter/logged_in/add_stock/AddStockController.java
@@ -1,9 +1,13 @@
 package interface_adapter.logged_in.add_stock;
 
 public class AddStockController {
-    private final AddStockInteractor addStockInteractor;
+    private final AddStockInputBoundary addStockInteractor;
 
-    public AddStockController(AddStockInteractor addStockInteractor) {
+    public AddStockController(AddStockInputBoundary addStockInteractor) {
         this.addStockInteractor = addStockInteractor;
+    }
+
+    public void execute(String ticker, String date, String amount) {
+        
     }
 }
