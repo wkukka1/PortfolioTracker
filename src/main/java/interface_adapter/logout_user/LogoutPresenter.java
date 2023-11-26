76480f8d1b8fd83c04d363139b91f593968ed44d
Will Kukkamalla <dogplate.jr@gmail.com>
2023-11-25 19:38:37 -0500

    Added the logic for the interface adapters of the logout usecase

--- src/main/java/interface_adapter/logout_user/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout_user/LogoutPresenter.java
@@ -1,4 +1,48 @@
 package interface_adapter.logout_user;
 
-public class LogoutPresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInState;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginViewModel;
+import use_case.logout.LogoutOutputBoundary;
+
+
+import javax.swing.*;
+
+public class LogoutPresenter implements LogoutOutputBoundary {
+    private final LoginViewModel loginViewModel;
+    private final LoggedInViewModel loggedInViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public LogoutPresenter(LoginViewModel loginViewModel, LoggedInViewModel loggedInViewModel
+            , ViewManagerModel viewManagerModel) {
+        this.loginViewModel = loginViewModel;
+        this.loggedInViewModel = loggedInViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView() {
+        LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setUsername(null);
+        this.loggedInViewModel.setState(loggedInState);
+        this.loggedInViewModel.setLoggedInUser(null);
+        this.loggedInViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(loginViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+
+    }
+
+    @Override
+    public void prepareFailView() {
+        JFrame frame = new JFrame("Logout Error");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a JOptionPane with an error message
+        JOptionPane.showMessageDialog(frame,
+                "An error occurred while logging out.",
+                "Logout Error",
+                JOptionPane.ERROR_MESSAGE);
+    }
 }
--- src/main/java/interface_adapter/logout_user/LogoutViewModel.java
+++
@@ -1,4 +0,0 @@
-package interface_adapter.logout_user;
-
-public class LogoutViewModel {
-}
