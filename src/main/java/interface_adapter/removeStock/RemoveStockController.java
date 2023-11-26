2e2042f8388ed10d99fe797e4d7f5d5d182b3502
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-11-26 17:06:59 -0500

    implemented the Controller initializer and the execute method that calls the InputBoundary method for the use case interactor

--- src/main/java/interface_adapter/removeStock/RemoveStockController.java
+++ src/main/java/interface_adapter/removeStock/RemoveStockController.java
@@ -1,2 +1,18 @@
-package interface_adapter.removeStock;public class removeStockController {
+package interface_adapter.removeStock;
+
+import use_case.removeStock.RemoveStockInputBoundary;
+import entity.Stock;
+import use_case.removeStock.RemoveStockInputData;
+
+public class RemoveStockController {
+    final RemoveStockInputBoundary removeStockInteractor;
+
+    public RemoveStockController(RemoveStockInputBoundary removeStockInteractor){
+        this.removeStockInteractor = removeStockInteractor;
+    }
+
+    public void execute(Stock stock){
+        RemoveStockInputData input = new RemoveStockInputData(stock);
+        removeStockInteractor.execute(input);
+    }
 }
