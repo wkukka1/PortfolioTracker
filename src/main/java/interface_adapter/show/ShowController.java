606d2d3b35f3e95f85330fd3a22b4ee1ad8212ec
alexlee27 <alexlee7942@gmail.com>
2023-11-27 01:13:11 -0500

    Implemented interface adapter classes for show use case

--- src/main/java/interface_adapter/show/ShowController.java
+++ src/main/java/interface_adapter/show/ShowController.java
@@ -1,4 +1,18 @@
 package interface_adapter.show;
 
+import use_case.show.ShowInputBoundary;
+import use_case.show.ShowInputData;
+
 public class ShowController {
+    final ShowInputBoundary showUseCaseInteractor;
+
+
+    public ShowController(ShowInputBoundary showUseCaseInteractor) {
+        this.showUseCaseInteractor = showUseCaseInteractor;
+    }
+
+    public void execute(int userID) {
+        ShowInputData showInputData = new ShowInputData(userID);
+        showUseCaseInteractor.execute(showInputData);
+    }
 }
