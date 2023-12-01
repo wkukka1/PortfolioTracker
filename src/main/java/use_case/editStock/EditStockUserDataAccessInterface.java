bd3e9b06f43c6c3966792e96a2b57413a65013ef
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-12-01 16:11:39 -0500

    implemented the EditStockUserDataAccessInterface

--- src/main/java/use_case/editStock/EditStockUserDataAccessInterface.java
+++ src/main/java/use_case/editStock/EditStockUserDataAccessInterface.java
@@ -1,2 +1,5 @@
-package use_case.editStock;public interface EditStockUserDataAccessInterface {
+package use_case.editStock;
+import entity.User;
+public interface EditStockUserDataAccessInterface {
+    User get(String username);
 }
