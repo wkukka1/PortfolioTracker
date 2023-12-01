0141986f10a80bc4cf038051478b771342bf1792
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-12-01 16:53:04 -0500

    implemented the initializer for the presenter

--- src/main/java/interface_adapter/editStock/EditStockPresenter.java
+++ src/main/java/interface_adapter/editStock/EditStockPresenter.java
@@ -1,2 +1,21 @@
-package interface_adapter.editStock;public class EditStockPresenter {
+package interface_adapter.editStock;
+
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInState;
+import interface_adapter.logged_in.LoggedInViewModel;
+import use_case.editStock.EditStockOutputBoundary;
+
+public class EditStockPresenter implements EditStockOutputBoundary {
+    private final EditStockViewModel editStockViewModel;
+    private final LoggedInViewModel loggedInViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public EditStockPresenter(EditStockViewModel editStockViewModel,
+                              LoggedInViewModel loggedInViewModel,
+                              ViewManagerModel viewManagerModel){
+        this.editStockViewModel = editStockViewModel;
+        this.loggedInViewModel = loggedInViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
 }
