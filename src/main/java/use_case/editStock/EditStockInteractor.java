f70f43bcf47f4586e900541c116e95b482523c89
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-12-01 16:18:22 -0500

    implemented the execute method

--- src/main/java/use_case/editStock/EditStockInteractor.java
+++ src/main/java/use_case/editStock/EditStockInteractor.java
@@ -9,8 +9,8 @@ import entity.Portfolio;
 public class EditStockInteractor implements EditStockInputBoundary {
     final LoggedInViewModel loggedInViewModel;
     final EditStockOutputBoundary editStockPresenter;
-    final PortfolioDataAccessInterface portfolioDataAccessInterface;
-    final EditStockUserDataAccessInterface userDataAccessInterface;
+    final PortfolioDataAccessInterface portfolioDataAccessObject;
+    final EditStockUserDataAccessInterface userDataAccessObject;
 
     public EditStockInteractor(LoggedInViewModel loggedInViewModel,
                                EditStockOutputBoundary editStockPresenter,
@@ -18,9 +18,31 @@ public class EditStockInteractor implements EditStockInputBoundary {
                                EditStockUserDataAccessInterface userDataAccessInterface){
         this.loggedInViewModel = loggedInViewModel;
         this.editStockPresenter = editStockPresenter;
-        this.portfolioDataAccessInterface = portfolioDataAccessInterface;
-        this.userDataAccessInterface = userDataAccessInterface;
+        this.portfolioDataAccessObject = portfolioDataAccessInterface;
+        this.userDataAccessObject = userDataAccessInterface;
     }
 
+    /**
+     * Edits how many shares of a stock the user owns
+     * @param editStock Contains the Stock the user wants to change and the new amount
+     *              of shares they own in that stock.
+     */
+    public void execute(EditStockInputData editStock){
+        try {
+            String tickerSymbol = editStock.getTickerSymbol();
+            double newQuantity = editStock.getNewQuantity();
 
+            String username = loggedInViewModel.getLoggedInUser();
+            User user = userDataAccessObject.get(username);
+            int userId = user.getUserID();
+            Portfolio portfolio = portfolioDataAccessObject.getPortfolioByID(userId);
+
+            Stock stock = portfolio.getStockByTicker(tickerSymbol);
+            stock.setQuantity(newQuantity);
+            editStockPresenter.prepareSuccessView();
+        } catch (Exception e){
+            editStockPresenter.prepareFailView(e.toString());
+            System.out.println(e.toString());
+        }
+    }
 }
