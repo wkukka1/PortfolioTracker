68072f95fdc3b54a5ee8d8ccba3fabeb233a6535
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-12-01 14:00:29 -0500

    implemented the initializer for the interactor

--- src/main/java/use_case/editStock/EditStockInteractor.java
+++ src/main/java/use_case/editStock/EditStockInteractor.java
@@ -1,2 +1,26 @@
-package use_case.editStock;public class EditStockInteractor {
+package use_case.editStock;
+
+import interface_adapter.logged_in.LoggedInViewModel;
+import use_case.signup.PortfolioDataAccessInterface;
+import entity.User;
+import entity.Stock;
+import entity.Portfolio;
+
+public class EditStockInteractor implements EditStockInputBoundary {
+    final LoggedInViewModel loggedInViewModel;
+    final EditStockOutputBoundary editStockPresenter;
+    final PortfolioDataAccessInterface portfolioDataAccessInterface;
+    final EditStockUserDataAccessInterface userDataAccessInterface;
+
+    public EditStockInteractor(LoggedInViewModel loggedInViewModel,
+                               EditStockOutputBoundary editStockPresenter,
+                               PortfolioDataAccessInterface portfolioDataAccessInterface,
+                               EditStockUserDataAccessInterface userDataAccessInterface){
+        this.loggedInViewModel = loggedInViewModel;
+        this.editStockPresenter = editStockPresenter;
+        this.portfolioDataAccessInterface = portfolioDataAccessInterface;
+        this.userDataAccessInterface = userDataAccessInterface;
+    }
+
+
 }
