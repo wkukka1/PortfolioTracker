9622652ef3f7a251bbc7ac02d0ce59eebb3b5455
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-12-01 16:32:46 -0500

    implemented the execute method in the controller

--- src/main/java/interface_adapter/editStock/EditStockController.java
+++ src/main/java/interface_adapter/editStock/EditStockController.java
@@ -1,11 +1,17 @@
 package interface_adapter.editStock;
 
 import use_case.editStock.EditStockInputBoundary;
+import use_case.editStock.EditStockInputData;
 
 public class EditStockController {
     final EditStockInputBoundary editStockInteractor;
 
     public EditStockController(EditStockInputBoundary interactor) {
         this.editStockInteractor = interactor;
+    }
+
+    public void execute(String tickerSymbol, double newQuantity){
+        EditStockInputData input = new EditStockInputData(tickerSymbol, newQuantity);
+        editStockInteractor.execute(input);
     }
 }
