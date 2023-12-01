bd36e646d60e3b57233b0794ddb9a0c7e7f346df
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-12-01 15:48:07 -0500

    implemented the inputdata initializer and the methods needed for the Edit Stock use case

--- src/main/java/use_case/editStock/EditStockInputData.java
+++ src/main/java/use_case/editStock/EditStockInputData.java
@@ -1,2 +1,19 @@
-package use_case.editStock;public class EditStockInputData {
+package use_case.editStock;
+
+public class EditStockInputData {
+    private String tickerSymbol;
+    private double quantity;
+    public EditStockInputData(String tickerSymbol,
+                              double newQuantity){
+        this.tickerSymbol = tickerSymbol;
+        this.quantity = newQuantity;
+    }
+
+    public String getTickerSymbol(){
+        return this.tickerSymbol;
+    }
+
+    public double getNewQuantity(){
+        return this.quantity;
+    }
 }
