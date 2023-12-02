c2ba981558b415226d28749ceb24c194e86a88e7
David Zhang <zdavid4525@gmail.com>
2023-12-01 23:39:02 -0500

    implemented public AddStockInteractor.addStock method

--- src/main/java/use_case/add_stock/AddStockInteractor.java
+++ src/main/java/use_case/add_stock/AddStockInteractor.java
@@ -1,2 +1,28 @@
-package use_case.add_stock;public class AddStockInteractor {
+package use_case.add_stock;
+
+import entity.Stock;
+import use_case.signup.PortfolioDataAccessInterface;
+
+public class AddStockInteractor implements AddStockInputBoundary {
+    private final PortfolioDataAccessInterface portfolioDataAccessImpl;
+
+    public AddStockInteractor(PortfolioDataAccessInterface portfolioDataAccessImpl) {
+        this.portfolioDataAccessImpl = portfolioDataAccessImpl;
+    }
+
+    @Override
+    public void addStock(AddStockInputData addStockData) {
+        Stock newStock;
+        double newStockProfitToDate;
+        try {
+            newStock = createNewStock(addStockData);
+            newStockProfitToDate = calculateNewStockProfitToDate(newStock);
+        } catch (RuntimeException re) {
+            addStockPresenter.prepareNonSuccessView();
+        }
+
+        portfolioDataAccessImpl.addStockToPortfolioByID(addStockData.getUserID(), newStock, newStockProfitToDate);
+
+        addStockPresenter.prepareSuccessView();
+    }
 }
