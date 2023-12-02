27e865bee1101db7c1ed8d9e6e65d808e5750892
David Zhang <zdavid4525@gmail.com>
2023-12-02 15:03:43 -0500

    removed unused import

--- src/main/java/use_case/add_stock/AddStockOutputBoundary.java
+++ src/main/java/use_case/add_stock/AddStockOutputBoundary.java
@@ -1,7 +1,5 @@
 package use_case.add_stock;
 
-import java.util.Map;
-
 public interface AddStockOutputBoundary {
     void prepareSuccessView(AddStockOutputData addStockOutputData);
     void prepareNonSuccessView(String addStockErrorMsg);
