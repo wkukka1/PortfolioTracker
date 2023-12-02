b7b45aaa06f8992733b41f98035fea6e08b5074e
David Zhang <zdavid4525@gmail.com>
2023-12-02 15:06:37 -0500

    updated LoginOutputData to include ticker to quantity hashmap

--- src/main/java/use_case/login/LoginOutputData.java
+++ src/main/java/use_case/login/LoginOutputData.java
@@ -1,17 +1,22 @@
 package use_case.login;
 
+import java.util.Map;
+
 public class LoginOutputData {
 
     private final String username;
     private final int userID;
     private boolean useCaseFailed;
     private double netProfit;
+    private Map<String, Double> tickersToQuantities;
 
-    public LoginOutputData(String username, int userID, boolean useCaseFailed, double netProfit) {
+    public LoginOutputData(String username, int userID, boolean useCaseFailed, double netProfit,
+                           Map<String, Double> tickersToQuantities) {
         this.username = username;
         this.userID = userID;
         this.useCaseFailed = useCaseFailed;
         this.netProfit = netProfit;
+        this.tickersToQuantities = tickersToQuantities;
     }
 
     public String getUsername() {
@@ -28,5 +33,13 @@ public class LoginOutputData {
 
     public void setNetProfit(double netProfit) {
         this.netProfit = netProfit;
+    }
+
+    public Map<String, Double> getTickersToQuantities() {
+        return tickersToQuantities;
+    }
+
+    public void setTickersToQuantities(Map<String, Double> tickersToQuantities) {
+        this.tickersToQuantities = tickersToQuantities;
     }
 }
