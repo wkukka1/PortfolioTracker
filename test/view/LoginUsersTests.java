bb9b862507ef5dbd1214337cd881ae2f0aaceb88
Will Kukkamalla <dogplate.jr@gmail.com>
2023-12-03 14:34:42 -0500

    Changed it so that it covers more of the code

--- test/view/LoginUsersTests.java
+++ test/view/LoginUsersTests.java
@@ -1,24 +1,19 @@
 package view;
 
+import app.Main;
 import data_access.FilePortfolioDataAccessObject;
 import data_access.FileUserDataAccessObject;
 import entity.CommonUserFactory;
 import entity.Portfolio;
 import entity.UserFactory;
-import interface_adapter.ViewManagerModel;
-import interface_adapter.logged_in.LoggedInViewModel;
-import interface_adapter.login.LoginController;
-import interface_adapter.login.LoginPresenter;
-import interface_adapter.login.LoginViewModel;
-import interface_adapter.signup.SignupController;
-import interface_adapter.signup.SignupPresenter;
-import interface_adapter.signup.SignupViewModel;
-import use_case.login.*;
-import use_case.signup.*;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+
+import static org.junit.Assert.assertNotNull;
 
 public class LoginUsersTests {
 
@@ -38,39 +33,102 @@ public class LoginUsersTests {
         }
     }
 
-    private boolean loginUser(String username, String password) throws IOException {
-        ViewManagerModel viewManagerModel = new ViewManagerModel();
-        LoginViewModel loginViewModel = new LoginViewModel();
-        LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
-        SignupViewModel signupViewModel = new SignupViewModel();
-        UserFactory userFactory = new CommonUserFactory();
-        try {
-            FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("./users.csv", userFactory);
-            PortfolioDataAccessInterface portfolioDataAccessInterface = new FilePortfolioDataAccessObject("./portfolios.csv");
-            SignupOutputBoundary signupPresenter = new SignupPresenter(viewManagerModel, signupViewModel, loginViewModel);
-            SignupInputBoundary signupInteractor = new SignupInteractor(userDataAccessObject, portfolioDataAccessInterface, signupPresenter, userFactory);
-            SignupController signupController = new SignupController(signupInteractor);
-            LoginOutputBoundary loginPresenter = new LoginPresenter(viewManagerModel, loggedInViewModel, loginViewModel, new SignupView(signupController, signupViewModel));
-            LoginInputBoundary loginInteractor = new LoginInteractor(userDataAccessObject, loginPresenter);
-            LoginController loginController = new LoginController(loginInteractor);
-            loginController.execute(username, password);
-            return loggedInViewModel.getLoggedInUser().equals(username);
-        } catch (Exception e) {
-            System.out.println("The file was not found");
+    private JButton getloginBtn(){
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame && ((JFrame) window).getTitle().equals("PortfolioTracker")) {
+                app = (JFrame) window;
+            }
         }
-        return false;
+        assertNotNull(app);
+
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        // Assuming LoginView is at index 1 in the JPanel
+        LoginView lv = (LoginView) jp2.getComponent(1);
+
+        JPanel buttons = (JPanel) lv.getComponent(5);
+
+        // Assuming logIn button is at index 0 in the buttons JPanel
+        return (JButton) buttons.getComponent(0);
+    }
+
+    private LabelTextPanel[] getTextfeilds(){
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame && ((JFrame) window).getTitle().equals("PortfolioTracker")) {
+                app = (JFrame) window;
+            }
+        }
+        assertNotNull(app);
+
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        // Assuming LoginView is at index 1 in the JPanel
+        LoginView lv = (LoginView) jp2.getComponent(1);
+
+        LabelTextPanel usernameTextField = (LabelTextPanel) lv.getComponent(1);
+        LabelTextPanel passwordTextField = (LabelTextPanel) lv.getComponent(3);
+        // Assuming logIn button is at index 0 in the buttons
+        return new LabelTextPanel[]{usernameTextField, passwordTextField};
+    }
+
+
+    private boolean loginUser(String username, String password) {
+        LabelTextPanel[] textfield = getTextfeilds();
+        LabelTextPanel usernameInputField = textfield[0];
+        LabelTextPanel passwordInputField = textfield[1];
+
+        usernameInputField.setText(username);
+        passwordInputField.setText(password);
+
+        getloginBtn().doClick();
+
+        boolean result = getloggedInUser().equals(username);
+
+        System.out.println("test");
+        return result;
+    }
+
+    private String getloggedInUser() {
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window:windows){
+            if(window instanceof JFrame && ((JFrame) window).getTitle().equals("PortfolioTracker")){
+                app = (JFrame) window;
+            }
+        }
+
+        assertNotNull(app);
+
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        // Assuming LoggedInView is at index 1 in the JPanel
+        LoggedInView lv = (LoggedInView) jp2.getComponent(2);
+
+        return lv.loggedInViewModel.getLoggedInUser();
     }
 
 
     @org.junit.Test
     public void loginOneUserTest() throws IOException {
         addUser(1);
+        Main.main(null);
         assert loginUser("user0", "password0");
     }
 
     @org.junit.Test
     public void loginMultipleUsersTest() throws IOException {
         addUser(5);
+        Main.main(null);
         assert loginUser("user0", "password0");
         assert loginUser("user1", "password1");
         assert loginUser("user2", "password2");
