2b2e77cdc5307b1e9153d9870dfea9241f22a4f6
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2023-12-04 21:33:50 -0500

    changed the outputdata to also take in the tickers to quantities map

--- src/main/java/use_case/editStock/EditStockOutputData.java
+++ src/main/java/use_case/editStock/EditStockOutputData.java
@@ -2,14 +2,22 @@ package use_case.editStock;
 
 import entity.Stock;
 
+import java.util.Map;
+
 public class EditStockOutputData {
     private Stock stock;
 
-    EditStockOutputData(Stock stock){
+    private Map<String, Double> tickersToAggregatedQuantities;
+    EditStockOutputData(Stock stock, Map<String, Double> tickersToAggregatedQuantities){
         this.stock = stock;
+        this.tickersToAggregatedQuantities = tickersToAggregatedQuantities;
     }
 
     public Stock getStock(){
         return this.stock;
+    }
+
+    public Map<String, Double> getTickersToAggregatedQuantities(){
+        return this.tickersToAggregatedQuantities;
     }
 }
