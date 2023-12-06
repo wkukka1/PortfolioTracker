0a3d506b984e86a13b2b6e9e9edbcc90ee5c0a1e
Will Kukkamalla <dogplate.jr@gmail.com>
2023-12-06 01:25:51 -0500

    Deleted Stock creator method in AddStockInteractor

--- src/main/java/use_case/add_stock/AddStockInteractor.java
+++ src/main/java/use_case/add_stock/AddStockInteractor.java
@@ -17,6 +17,7 @@ public class AddStockInteractor implements AddStockInputBoundary {
     private final PortfolioDataAccessInterface portfolioDataAccessImpl;
     private final StockCalculationService stockCalculationServiceImpl;
     private final AddStockOutputBoundary addStockPresenter;
+    private final StockFactory stockFactory;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private final String ADD_STOCK_DEFAULT_ERROR = "There was an issue adding this stock.\n Please check the input " +
             "fields and try again.";
@@ -31,6 +32,7 @@ public class AddStockInteractor implements AddStockInputBoundary {
         this.portfolioDataAccessImpl = portfolioDataAccessImpl;
         this.addStockPresenter = addStockPresenter;
         this.stockCalculationServiceImpl = stockCalculationServiceImpl;
+        this.stockFactory = new StockFactory(stockPriceClientImpl);
     }
 
     @Override
@@ -39,7 +41,7 @@ public class AddStockInteractor implements AddStockInputBoundary {
         double newStockProfitToDate;
         double overallNetProfit;
         try {
-            newStock = createNewStock(addStockData);
+            newStock = stockFactory.createStock(addStockData);
             newStockProfitToDate = stockCalculationServiceImpl.calculateNewStockProfitToDate(newStock);
             overallNetProfit = portfolioDataAccessImpl.getPortfolioByID(addStockData.getUserID()).getNetProfit() +
                     newStockProfitToDate;
@@ -59,17 +61,6 @@ public class AddStockInteractor implements AddStockInputBoundary {
         AddStockOutputData addStockOutputData = new AddStockOutputData(round(overallNetProfit, 2),
                 tickersToQuantities);
         addStockPresenter.prepareSuccessView(addStockOutputData);
-    }
-
-    private Stock createNewStock(AddStockInputData addStockData) throws IOException, IllegalArgumentException {
-        String purchaseDate = addStockData.getPurchaseLocalDateTime().format(formatter);
-
-        double pastStockClosePrice = stockPriceClientImpl.getStockInfoByDate(
-                addStockData.getTickerSymbol(), purchaseDate).getClose();
-
-        double newStockQuantity = addStockData.getTotalValueAtPurchase() / pastStockClosePrice;
-        return new Stock(addStockData.getTickerSymbol(), addStockData.getPurchaseLocalDateTime(), newStockQuantity,
-                addStockData.getTotalValueAtPurchase());
     }
 
     private double round(double value, int places) {
