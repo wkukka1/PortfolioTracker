db48a1531b61379cfc70f9ebcf6690d6fe6eaaf5
Will Kukkamalla <dogplate.jr@gmail.com>
2023-12-06 17:54:50 -0500

    Created Tests for new addStockInteractor

--- src/test/java/use_case/add_stock/AddStockInteractorTest.java
+++ src/test/java/use_case/add_stock/AddStockInteractorTest.java
@@ -4,6 +4,7 @@ import data_access.FilePortfolioDataAccessObject;
 import data_access.StockInfoClient;
 import entity.AVTimeSeriesDailyResponse;
 import entity.Portfolio;
+import entity.Short;
 import entity.Stock;
 import interface_adapter.logged_in.add_stock.AddStockPresenter;
 import org.junit.Before;
@@ -57,6 +58,31 @@ public class AddStockInteractorTest {
         verify(mockStockCalcService, times(1)).calculateNewStockProfitToDate(any(Stock.class));
         verify(mockPortfolioDAO, times(2)).getPortfolioByID(1);
         verify(mockPortfolioDAO, times(1)).addStockToPortfolioByID(anyInt(), any(Stock.class),
+                anyDouble());
+        verify(mockPresenter, times(1)).prepareSuccessView(any(AddStockOutputData.class));
+    }
+
+    @Test
+    public void testValidAddShortCallCallsNecessaryServices() throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+        String nowStr = now.format(formatter);
+        AddStockInputData mockInputData = new AddStockInputData("AAPL", now,
+                200.0, 1, "Short");
+        AVTimeSeriesDailyResponse mockClientRes = new AVTimeSeriesDailyResponse("2023-12-01", 1, 1,
+                1, 200.0, 1);
+        Portfolio mockPortfolioRes = new Portfolio(new ArrayList<>(), 0, 1);
+        Map<String, Double> mockTToQ = new HashMap<>();
+        mockTToQ.put("AAPL", 1.0);
+
+        when(mockStockClient.getStockInfoByDate(anyString(), anyString())).thenReturn(mockClientRes);
+        when(mockStockCalcService.calculateNewShortProfitToDate(any(Short.class))).thenReturn(69.0);
+        when(mockPortfolioDAO.getPortfolioByID(anyInt())).thenReturn(mockPortfolioRes);
+
+        addStockInteractor.addStock(mockInputData);
+        verify(mockStockClient, times(1)).getStockInfoByDate("AAPL", nowStr);
+        verify(mockStockCalcService, times(1)).calculateNewShortProfitToDate(any(Short.class));
+        verify(mockPortfolioDAO, times(2)).getPortfolioByID(1);
+        verify(mockPortfolioDAO, times(1)).addStockToPortfolioByID(anyInt(), any(Short.class),
                 anyDouble());
         verify(mockPresenter, times(1)).prepareSuccessView(any(AddStockOutputData.class));
     }
