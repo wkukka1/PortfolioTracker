300307339a8b1255441307690e6cdf1d912c45d0
David Zhang <zdavid4525@gmail.com>
2023-12-05 21:58:57 -0500

    added tests for StockCalculationServiceImpl

---
+++ src/test/java/use_case/add_stock/StockCalculationServiceImplTest.java
@@ -0,0 +1,87 @@
+package use_case.add_stock;
+
+import data_access.StockPriceClientImpl;
+import entity.AVTimeSeriesDailyResponse;
+import entity.Stock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
+public class StockCalculationServiceImplTest {
+    private final StockPriceClientImpl mockClient = Mockito.mock(StockPriceClientImpl.class);
+    private StockCalculationService mockStockCalculationServiceImpl;
+    private MockedStatic<LocalDateTime> mockLocalDateTime;
+
+    @Before
+    public void setup() {
+        this.mockStockCalculationServiceImpl = new StockCalculationServiceImpl(mockClient);
+        this.mockLocalDateTime = Mockito.mockStatic(LocalDateTime.class, Mockito.CALLS_REAL_METHODS);
+    }
+
+    @After
+    public void teardown() {
+        mockLocalDateTime.close();
+    }
+
+    @Test
+    public void testCalculateNewStockProfitToDateForNonSatSunMonDays() throws IOException {
+        Stock newStock = new Stock("AAPL", LocalDateTime.now(), 1.0, 100.0);
+        AVTimeSeriesDailyResponse mockRes = new AVTimeSeriesDailyResponse("", 1.0, 1.0, 1.0,
+                420.0, 1);
+
+        Mockito.when(mockClient.getStockInfoByDate(anyString(), anyString())).thenReturn(mockRes);
+        double actual = mockStockCalculationServiceImpl.calculateNewStockProfitToDate(newStock);
+
+        assertEquals(320.0, actual, 0);
+    }
+
+    @Test
+    public void testCalculateNewStockProfitToDateForMondays() throws IOException {
+        Stock newStock = new Stock("AAPL", LocalDateTime.now(), 1.0, 100.0);
+        AVTimeSeriesDailyResponse mockRes = new AVTimeSeriesDailyResponse("", 1.0, 1.0, 1.0,
+                69.0, 1);
+        LocalDateTime mockDate = LocalDateTime.of(2023, 12, 4, 0, 0);
+
+        mockLocalDateTime.when(LocalDateTime::now).thenReturn(mockDate);
+        Mockito.when(mockClient.getStockInfoByDate(anyString(), anyString())).thenReturn(mockRes);
+
+        double actual = mockStockCalculationServiceImpl.calculateNewStockProfitToDate(newStock);
+        assertEquals(-31.0, actual, 0);
+    }
+
+    @Test
+    public void testCalculateNewStockProfitToDateForSundays() throws IOException {
+        Stock newStock = new Stock("AAPL", LocalDateTime.now(), 1.0, 100.0);
+        AVTimeSeriesDailyResponse mockRes = new AVTimeSeriesDailyResponse("", 1.0, 1.0, 1.0,
+                69.0, 1);
+        LocalDateTime mockDate = LocalDateTime.of(2023, 12, 3, 0, 0);
+
+        mockLocalDateTime.when(LocalDateTime::now).thenReturn(mockDate);
+        Mockito.when(mockClient.getStockInfoByDate(anyString(), anyString())).thenReturn(mockRes);
+
+        double actual = mockStockCalculationServiceImpl.calculateNewStockProfitToDate(newStock);
+        assertEquals(-31.0, actual, 0);
+    }
+
+    @Test
+    public void testCalculateNewStockProfitToDateForSaturdays() throws IOException {
+        Stock newStock = new Stock("AAPL", LocalDateTime.now(), 1.0, 100.0);
+        AVTimeSeriesDailyResponse mockRes = new AVTimeSeriesDailyResponse("", 1.0, 1.0, 1.0,
+                69.0, 1);
+        LocalDateTime mockDate = LocalDateTime.of(2023, 12, 2, 0, 0);
+
+        mockLocalDateTime.when(LocalDateTime::now).thenReturn(mockDate);
+        Mockito.when(mockClient.getStockInfoByDate(anyString(), anyString())).thenReturn(mockRes);
+
+        double actual = mockStockCalculationServiceImpl.calculateNewStockProfitToDate(newStock);
+        assertEquals(-31.0, actual, 0);
+    }
+}
